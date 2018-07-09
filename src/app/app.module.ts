3c7ad3c3f8ae1c7dcece5a60e955886448cf9cb1
Brandon Kelton <brandon.kelton@outlook.com>
2018-07-08 18:56:07 -0600

    pulling together the Services page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './/app-routing.module';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -24,6 +24,7 @@ import { GalleryComponent } from './gallery/gallery.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -7,7 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
+  private expandText = 'Expand';
+  private collapseText = 'Collapse';
+
   constructor() { }
+
+  private _isWaterScapesExpanded: boolean = false;
+  get isWaterScapesExpanded(): boolean {
+    return this._isWaterScapesExpanded;
+  }
+  set isWaterScapesExpanded(x: boolean) {
+    this._isWaterScapesExpanded = x;
+  }
+  isLiquiscapeYourLifeExpanded = false;
+
+  toggleWaterScapesExpanded(): void {
+    console.log(this.isWaterScapesExpanded);
+    this.isWaterScapesExpanded = !this.isWaterScapesExpanded;
+    console.log(this.isWaterScapesExpanded);
+  }
+
+  toggleLiquiscapeYourLifeExpanded(): void {
+    this.isLiquiscapeYourLifeExpanded = !this.isLiquiscapeYourLifeExpanded;
+  }
+
+  get waterScapesExpansionText(): string {
+    return this.isWaterScapesExpanded ? this.collapseText : this.expandText;
+  }
+
+  get liquiscapeYourLifeExpansionText(): string {
+    return this.isLiquiscapeYourLifeExpanded ? this.collapseText : this.expandText;
+  }
 
   ngOnInit() {
   }
