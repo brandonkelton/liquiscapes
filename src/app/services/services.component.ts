f953db9d6bba8d2379b0f247ac0e10b55b8f4555
Brandon Kelton <brandon.kelton@outlook.com>
2018-07-09 19:36:16 -0600

    got animation working on services page

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,30 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.css']
+  styleUrls: ['./services.component.css'],
+  animations: [
+    trigger('expandCollapse', [
+      // transition(':enter', [
+      //   animate('500ms ease-in')
+      // ]),
+      // transition(':leave', [
+      //   animate('500ms ease-out')
+      // ]),
+      // state('*', style({}))
+      state('open', style({
+          height: '*',
+          opacity: 1
+      })),
+      state('close', style({
+          height: '0px',
+          opacity: 0
+      })),
+      transition('close => open', animate('500ms ease-in')),
+      transition('open => close', animate('500ms ease-out'))
+    ])
+  ]
 })
 export class ServicesComponent implements OnInit {
 
-  private expandText = 'Expand';
-  private collapseText = 'Collapse';
+  private expandText = 'More Information';
+  private collapseText = 'Less Information';
 
   constructor() { }
 
-  private _isWaterScapesExpanded: boolean = false;
-  get isWaterScapesExpanded(): boolean {
-    return this._isWaterScapesExpanded;
-  }
-  set isWaterScapesExpanded(x: boolean) {
-    this._isWaterScapesExpanded = x;
-  }
+  isWaterScapesExpanded = false;
   isLiquiscapeYourLifeExpanded = false;
 
   toggleWaterScapesExpanded(): void {
-    console.log(this.isWaterScapesExpanded);
     this.isWaterScapesExpanded = !this.isWaterScapesExpanded;
-    console.log(this.isWaterScapesExpanded);
   }
 
   toggleLiquiscapeYourLifeExpanded(): void {
