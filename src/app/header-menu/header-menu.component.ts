199fd840bc146a2d998b1f096a33a4f1c0efeddc
Brandon Kelton <brandon.kelton@outlook.com>
2018-07-25 19:35:32 -0600

    finished new custom menu!  :)  :)  :)

--- src/app/header-menu/header-menu.component.ts
+++ src/app/header-menu/header-menu.component.ts
@@ -9,6 +9,20 @@ export class HeaderMenuComponent implements OnInit {
 
   constructor() { }
 
+  get smallMenuClasses(): string {
+    return this._isVisible ? 'small-menu-popup' : 'small-menu-popup small-menu-hidden';
+  }
+
+  private _isVisible: boolean = false;
+
+  toggleSmallMenuVisibility() {
+    this._isVisible = !this._isVisible;
+  }
+
+  hideSmallMenu(): void {
+    this._isVisible = false;
+  }
+
   ngOnInit() {
   }
 
